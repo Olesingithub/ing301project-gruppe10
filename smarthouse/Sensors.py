b1ba8fc3e2f712cf6ef50cd10a8733e6d828fcac
Ole-Andre Pettersen <olesingithub>
2025-02-23 21:34:19 +0100

    deleted sensors.py

--- smarthouse/Sensors.py
+++
@@ -1,9 +0,0 @@
-
-class Sensor:
-    def __init__(self, sensorID, sensorType, sensorValue):
-        self.sensorID = sensorID
-        self.sensorType = sensorType
-        self.sensorValue = sensorValue
-
-    def getSensorValue(self):
-        self.sensorValue =
